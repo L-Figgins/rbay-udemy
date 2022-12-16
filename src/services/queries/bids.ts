01cfa4b502af0500426663f74553ad84231d83ca
Logan Figgins <l.figgins.se@gmail.com>
2022-12-16 14:57:00 -0800

    bids

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,7 +1,30 @@
-import type { CreateBidAttrs, Bid } from '$services/types';
+import type { CreateBidAttrs,} from '$services/types';
+import { client} from "$services/redis";
+import { DateTime } from 'luxon';
+import { parse } from 'dotenv';
+import { bidHistoryKey } from '$services/keys';
 
-export const createBid = async (attrs: CreateBidAttrs) => {};
+
+export const createBid = async (attrs: CreateBidAttrs) => {
+	const serialized = serializeHistory(attrs.amount,attrs.createdAt.toMillis())
+
+	return client.rPush(bidHistoryKey(attrs.itemId, seril))
+}
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	return [];
 };
+
+
+const serializeHistory = (amount:number, createdAt:number)=> {
+	return `${amount}:${createdAt}`
+}
+
+const deserializeHistory = (stored:string) => {
+	const [amount, createdAt] = stored.split(':');
+
+	return {
+		amount: parseFloat(amount),
+		createdAt: DateTime.fromMillis(parseInt(createdAt))
+	}
+}
