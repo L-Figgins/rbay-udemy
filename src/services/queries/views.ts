d266d0673f6eac1af8f8b8a0bcde1d6ad540fa29
Logan Figgins <l.figgins.se@gmail.com>
2023-01-11 12:46:40 -0800

    call incrementView

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,15 +1,27 @@
-import { itemsByViewsKey, itemsKey, itemsViewsKey } from "$services/keys";
 import { client } from "$services/redis";
 
 export const incrementView = async (itemId: string, userId: string) => {
     //hyperlog
-    const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
 
-    if(inserted){
-        return Promise.all([
-            client.hIncrBy(itemsKey(itemId),'views', 1),
-            // score is the first arguement in zsets swapped from above
-            client.zIncrBy(itemsByViewsKey(),1,itemId)
-        ])
-    }
+    //before lua script optimization
+    // const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+
+    // if(inserted){
+    //     return Promise.all([
+    //         client.hIncrBy(itemsKey(itemId),'views', 1),
+    //         // score is the first arguement in zsets swapped from above
+    //         client.zIncrBy(itemsByViewsKey(),1,itemId)
+    //     ])
+    // }
+
+    // keys needed for script
+    // 1) itemsViewsKey
+    // 2) itemsKey
+    // 3) itemsByViewsKey
+
+    // args
+    // itemId
+    // userId
+
+    return client.incrementView(itemId, userId)
 };
