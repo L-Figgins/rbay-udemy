285d3a05dfe098c808272dccfc94471728312668
Logan Figgins <l.figgins.se@gmail.com>
2023-01-11 12:33:20 -0800

    add code to correct client

--- src/services/redis/client.ts
+++ src/services/redis/client.ts
@@ -1,11 +1,29 @@
-import { createClient } from 'redis';
+import { createClient , defineScript} from 'redis';
+import {itemsKey, itemsViewsKey, itemsByViewsKey} from "$services/keys";
 
 const client = createClient({
 	socket: {
 		host: process.env.REDIS_HOST,
 		port: parseInt(process.env.REDIS_PORT)
 	},
-	password: process.env.REDIS_PW
+	password: process.env.REDIS_PW,
+	scripts: {
+		addOneAndStore: defineScript({
+				NUMBER_OF_KEYS: 1,
+				SCRIPT: `
+					return redis.call('SET', KEYS[1], 1 + tonumber(ARGV[1]))
+					`,
+				transformArguments(key:string, value:number) {
+						// this functions transforms provided arguments into something
+						// that can be consumed by evalsha 
+						// in this case converting the number to a string
+						return [key, value.toString()]
+				},
+					transformReply(reply:any){
+						return reply
+			}
+		})
+	}
 });
 
 client.on('error', (err) => console.error(err));
