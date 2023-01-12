8be90dccd79e7d5b10f6c7b739c15d4cfb02404b
Logan Figgins <l.figgins.se@gmail.com>
2023-01-12 13:03:30 -0800

    implement simple lock. Use redilock in real projects

--- src/services/redis/lock.ts
+++ src/services/redis/lock.ts
@@ -1,4 +1,40 @@
-export const withLock = async () => {};
+import { client } from "$services/redis";
+import { randomBytes} from "crypto";
+
+export const withLock = async (key:string, cb:()=>any) => {
+	// use redi-lock for real projects this is just an example of locking
+
+	// rety defaults
+	const retryDelayMs = 100;
+	let retries = 20;
+
+	const token = randomBytes(6).toString('hex')
+	const lockKey = `lock:${key}`
+
+	while (retries > 0) {
+		//acquire lock
+		retries--;
+		const acquired = await client.set(lockKey, token, {
+			NX: true
+		})
+		
+		// if not is not acquired retry
+
+		if(!acquired){
+			await pause(retryDelayMs);
+			continue
+		}
+
+		const result = await cb()
+		//unset lock
+
+		await client.del(lockKey)
+		return result;
+	}
+
+
+	//generate a random value
+};
 
 const buildClientProxy = () => {};
 
