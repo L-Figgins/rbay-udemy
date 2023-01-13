e7a43b05e1caf67de21324cdec6a7ff07245073f
Logan Figgins <l.figgins.se@gmail.com>
2023-01-13 15:53:22 -0800

    implement search using redisearch

--- src/services/queries/items/search.ts
+++ src/services/queries/items/search.ts
@@ -1,1 +1,36 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+import {client} from "$services/redis";
+import { deserialize } from "./deserialize";
+import { itemsIndexKey } from "$services/keys";
+
+export const searchItems = async (term: string, size: number = 5) => {
+    //find all non-alphanumeric characters and replace with an empty string
+    const cleaned = term.replaceAll(/[^a-zA-Z0-9]/g, '').trim().split(' ').map((word)=> word ? `%${word}%` : '').join(' ')
+
+    // Look at cleaned and make sure it is valid
+
+    // use the client to do an actual search
+
+    // deserialize and return the search results
+
+
+    if(cleaned === '') {
+        return [];
+    }
+
+    const results = await client.ft.search(
+        itemsIndexKey(),
+        //since we searching against all fields we don't need @field syntax
+        cleaned, 
+        {
+            LIMIT: {
+                from: 0,
+                size,
+            }
+        }
+    )
+
+    //deserialize and return the search results
+
+    return results.documents.map( ({id, value}) => deserialize(id, value as any))
+
+}; 
