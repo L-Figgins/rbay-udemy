c8022b3179ecd0dd2e6228141afe8dc4a9461173
Logan Figgins <l.figgins.se@gmail.com>
2023-01-16 15:16:53 -0800

    implement by user query with sortable fields

--- src/services/queries/items/by-user.ts
+++ src/services/queries/items/by-user.ts
@@ -1,4 +1,6 @@
 import { client } from "$services/redis";
+import { itemsIndexKey } from "$services/keys";
+import { deserialize } from "./deserialize";
 
 interface QueryOpts {
 	page: number;
@@ -8,5 +10,33 @@ interface QueryOpts {
 }
 
 export const itemsByUser = async (userId: string, opts: QueryOpts) => {
-	return []
+	const query = `@ownerId:{${userId}}`
+
+	const sortCriteria = opts.sortBy && opts.direction && {
+		BY: opts.sortBy, DIRECTION: opts.direction
+	}
+
+	const {total, documents} = await client.ft.search(
+		itemsIndexKey(),
+		query,
+		{
+			ON: 'HASH',
+			SORTBY: sortCriteria,
+			LIMIT: {
+				from: opts.page * opts.perPage,
+				size: opts.perPage
+			}
+		} as any
+	);
+
+
+	return {
+		totalPages: Math.ceil(total/ opts.perPage),
+		items: documents.map(({id, value}) => {
+			return deserialize(
+				id.replace('items#', ''),
+				value as any
+			)
+		})
+	}
 };
